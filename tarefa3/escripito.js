3b6b40b151b401e155536cd8756f2c1e33bacdc7
rubenfr77 <94474585+rubenfr77@users.noreply.github.com>
2021-12-10 13:53:34 +0100

    Add files via upload

---
+++ tarefa3/escripito.js
@@ -0,0 +1,23 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+  
+
+
+
+
+var moito = 234.56
+var dous = 100.33
+var tres = 120.12
+var cuatro = 231.22
+var raiz =Math.sqrt(moito);
+ alert(raiz);
+ var min = Math.min(moito, tres);
+ alert(min);
+ var cuadrado = Math.pow (cuatro, 5);
+ alert(cuadrado);
+ alert(Math.PI);
+ alert(Math.random(1, 3));
+ 
